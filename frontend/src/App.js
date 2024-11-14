f7c2f07c1c9aa6900d596a96b531cc7f24964bf5
Adi Jain <114348511+Adij003@users.noreply.github.com>
2024-11-14 10:58:00 +0530

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Routes>
         <Route path="/" element={<LoginPage/>}/>
-
+      <Route path="/home" element={<Home/>}/>
         </Routes>
       </Router>
      
